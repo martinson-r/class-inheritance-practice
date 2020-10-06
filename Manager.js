2d9475161872e9013ef71bcf8e6864a5b9e35fd2
William Vincent <compelledtograce@gmail.com>
2020-10-06 18:01:08 -0400

    progress has been made?

--- Manager.js
+++ Manager.js
@@ -1,25 +1,36 @@
-const {Employee} = require('./Employee');
+const { Employee } = require("./Employee");
 
 class Manager extends Employee {
-    constructor(name, title, salary, boss) {
+  constructor(name, title, salary, boss) {
     super(name, title, salary, boss);
-    this.employeeArray = [];
-    }
+    this.employeeArray = [...];
+  }
 
+  //for loop of Employee array
+  //if statement which checks if employee array at index is an instance of Manager
+  //inside if statement, we push each element of the employee array that is an instance
+  //of Manager into a new array
+  //run reduce on that array
 
-    //for loop of Employee array
-    //if statement which checks if employee array at index is an instance of Manager
-    //inside if statement, we push each element of the employee array that is an instance
-    //of Manager into a new array
-    //run reduce on that array
-
-    bonus(multiplier) {
-
-        if () {
-        this.totalSalaries = employeeArray.reduce ((salaries, currentElement) => {
-            return salaries+= currentElement.salary;
-        });
-    }
-        this.managerBonus = totalSalaries * multiplier;
-    }
+  bonus(multiplier) {
+    const sumSalaries = () => {
+      if (!this.employeeArray.length) {
+        let reducedSalary = (this.totalSalaries = employeeArray.reduce(
+          (salaries, currentElement) => {
+            if (this.name === currentElement.boss) {
+              return (salaries += currentElement.salary);
+            }
+          }
+        ));
+        sumSalaries();
+      } else {
+        return reducedSalary;
+      }
+      this.managerBonus = reducedSalary * multiplier;
+    };
+  }
 }
+
+//upper level manager
+//mid level
+//low level employee
