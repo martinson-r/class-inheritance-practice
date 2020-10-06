78f4673d0db678f55f35f38601bb0d15cee6aa2b
Rihana Chang <martinson.r@gmail.com>
2020-10-06 16:03:32 -0700

    Fixed bug in subsalaries calculator

--- Manager.js
+++ Manager.js
@@ -1,36 +1,43 @@
 const { Employee } = require("./Employee");
 
 class Manager extends Employee {
-  constructor(name, title, salary, boss) {
-    super(name, title, salary, boss);
-    this.employeeArray = [...];
-  }
+    constructor(name, title, salary, boss) {
+        super(name, title, salary, boss);
+        this.employeeArray = [];
+    }
 
-  //for loop of Employee array
-  //if statement which checks if employee array at index is an instance of Manager
-  //inside if statement, we push each element of the employee array that is an instance
-  //of Manager into a new array
-  //run reduce on that array
+    addEmployee(employee) {
+        this.employeeArray.push(employee);
+    }
 
-  bonus(multiplier) {
-    const sumSalaries = () => {
-      if (!this.employeeArray.length) {
-        let reducedSalary = (this.totalSalaries = employeeArray.reduce(
-          (salaries, currentElement) => {
-            if (this.name === currentElement.boss) {
-              return (salaries += currentElement.salary);
-            }
-          }
-        ));
-        sumSalaries();
-      } else {
-        return reducedSalary;
-      }
-      this.managerBonus = reducedSalary * multiplier;
+    bonus(multiplier) {
+        if (this.employeeArray.length) {
+            const totalSalaries = this.totalSubSalaries();
+            this.managerBonus = totalSalaries * multiplier;
+            return this.managerBonus;
+        }
     };
-  }
+    totalSubSalaries() {
+        return this.employeeArray.reduce(
+            (salaries, currentEmployee) => {
+                if (currentEmployee instanceof Manager) {
+                    return salaries + currentEmployee.salary + currentEmployee.totalSubSalaries();
+                }
+                return salaries + currentEmployee.salary;
+            }, 0);
+    }
+
 }
 
-//upper level manager
-//mid level
-//low level employee
+const Jodi = new Manager('Jodi', 'Human', 5000, null);
+const Lucky = new Employee('Lucky', 'Cat', 1000, Jodi);
+const Mike = new Manager('Mike', 'Janitor', 3000, Jodi);
+const Teddy = new Employee('Teddy', 'stuffed animal', 500, Mike);
+
+Jodi.addEmployee(Lucky);
+Jodi.addEmployee(Mike);
+Mike.addEmployee(Teddy);
+
+const JodiSalary = Jodi.bonus(.1);
+console.log(JodiSalary);
+
